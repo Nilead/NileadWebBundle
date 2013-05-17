c6b34831a0ddfae1c2ed1cedd875e8b0bd8cb8d3
Tuan Nguyen <tuannguyen@rubikin.com>
2013-05-17 18:00:55 +0700

    fix bug in NileadWebBundle class

--- NileadWebBundle.php
+++ NileadWebBundle.php
@@ -24,20 +24,5 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  */
 class NileadWebBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
-    {
-        $interfaces = array(
-            'Nilead\WebBundle\Model\ContainerInterface' => 'nilead.model.container.class',
-            'Nilead\WebBundle\Model\ContainerTypeInterface' => 'nilead.model.container_type.class',
-            'Nilead\WebBundle\Model\FacilityInterface' => 'nilead.model.facility.class',
-            'Nilead\WebBundle\Model\InventoryItemInterface' => 'nilead.model.inventory_item.class',
-            'Nilead\WebBundle\Model\InventoryItemStatusTypeInterface' => 'nilead.model.inventory_item_status_type.class',
-            'Nilead\WebBundle\Model\InventoryItemVarianceInterface' => 'nilead.model.inventory_item_variance.class',
-            'Nilead\WebBundle\Model\InventoryItemVarianceReasonInterface' => 'nilead.model.inventory_item_variance_reason.class',
-            'Nilead\WebBundle\Model\LocationInterface' => 'nilead.model.location.class',
-            'Nilead\WebBundle\Model\LotInterface' => 'nilead.model.lot.class',
-        );
 
-        $container->addCompilerPass(new ResolveDoctrineTargetEntitiesPass('nilead_inventory', $interfaces));
-    }
 }
