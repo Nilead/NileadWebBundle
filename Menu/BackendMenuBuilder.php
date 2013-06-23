405bde6b825129f11cec732855b24880b6c5e7f9
Vinh <vinhtrinh@rubikin.com>
2013-06-23 21:46:17 +0700

    Update 2013.06.23

---
+++ Menu/BackendMenuBuilder.php
@@ -0,0 +1,16 @@
+<?php
+ /**
+ * Created by Rubikin Team.
+ * Date: 6/12/13
+ * Time: 10:47 AM
+ * Question? Come to our website at http://rubikin.com
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nilead\WebBundle\Menu;
+
+
+class BackendMenuBuilder extends MenuBuilder {
+
+}
---
+++ Menu/FrontendMenuBuilder.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by Rubikin Team.
+ * Date: 6/12/13
+ * Time: 10:48 AM
+ * Question? Come to our website at http://rubikin.com
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Nilead\WebBundle\Menu;
+
+
+use Symfony\Component\HttpFoundation\Request;
+
+class FrontendMenuBuilder extends MenuBuilder
+{
+    public function createUserAccountMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('index', array('route' => 'nilead.frontend.account.index'));
+
+        return $menu;
+    }
+}
